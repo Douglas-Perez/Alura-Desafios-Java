50fcfb76b100ad16f5a3140f00a4df5fd3334ec9
Douglas-Perez <douglas.perez.c@hotmail.com>
2025-08-04 22:22:51 -0300

    chore: melhora formatação do JSON de Titulo com setPrettyPrinting
    Atualiza o programa de serialização da classe Titulo para gerar JSON formatado.
    Utiliza o método setPrettyPrinting da biblioteca Gson para gerar saída mais legível.

--- src/escritaDeArquivos/EscritaJson.java
+++ src/escritaDeArquivos/EscritaJson.java
@@ -1,10 +1,11 @@
 package escritaDeArquivos;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class EscritaJson {
     public static void main(String[] args) {
-        Gson gson = new Gson();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
         Titulo titulo = new Titulo("Interstellar", 2014, "Christopher Nolan");
 
